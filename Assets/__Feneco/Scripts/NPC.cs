7e98c5a7ac70f1a1ddb192f1fb35455cfa73ab45
Saulo Souza <126815810+SauloSouza27@users.noreply.github.com>
2024-04-10 14:41:06 -0300

    NPC config

--- Assets/__Feneco/Scripts/NPC.cs
+++ Assets/__Feneco/Scripts/NPC.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject dialogueScreen;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject questScreen;
+
+    [SerializeField] private Sprite avatar;
+
+    [SerializeField] private string[] dialogue;
+
+    [SerializeField] private GameObject callbackObject = null;
+
+    [SerializeField] private string metodo;
+
+    private Animator npc_fsm;
 }
