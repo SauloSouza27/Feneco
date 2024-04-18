bab0021e8418c7aff414bfb8b39bd4f6b79f64bb
Saulo Souza <126815810+SauloSouza27@users.noreply.github.com>
2024-04-18 01:56:10 -0300

    Enemy Coiote

--- Assets/__Feneco/Scripts/EnemyController.cs
+++ Assets/__Feneco/Scripts/EnemyController.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private EnemyBase enemy;
+    [SerializeField] private float HP;
+
+    [SerializeField] private int damage;
+
+    [SerializeField] private Sprite avatar;
+
+    [SerializeField] private Mesh[] meshs;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+    public void TakeDamage(int damage)
+    {
+        HP = -damage;
+    }
+    public void SetMesh(MeshFilter meshFilter, int index)
+    {
+        meshFilter.mesh = meshs[index];
+    }
 }
