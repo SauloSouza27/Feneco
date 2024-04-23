0e3dc7bb4cb57fd13a273f95b4121c959f8b4578
Krazik01 <ian.wb1@hotmail.com>
2024-04-23 16:49:07 -0300

    Targeting complete

--- Assets/__Development/Ian/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/__Development/Ian/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class PlayerTargetingState : PlayerBaseState
 {
 
     private readonly int TargetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+    private readonly int TargetingForwardHash = Animator.StringToHash("TargetingForwardSpeed");
+    private readonly int TargetingRightHash = Animator.StringToHash("TargetingRightSpeed");
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
@@ -17,10 +20,19 @@ public class PlayerTargetingState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-       if(stateMachine.Targeter.CurrentTarget == null)
-       {
+        if(stateMachine.Targeter.CurrentTarget == null)
+        {
             stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
-       }
+            return;
+        }
+        
+        Vector3 movement = CalculateMovement();
+
+        Move(movement * stateMachine.TargetingMovementSpeed, deltaTime);
+
+        UpdateAnimator(deltaTime);
+        
+        FaceTarget();
     }
 
     public override void Exit()
@@ -33,5 +45,39 @@ public class PlayerTargetingState : PlayerBaseState
         stateMachine.Targeter.Cancel();
 
         stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 movement = new Vector3();
+
+        movement += stateMachine.transform.right * stateMachine.InputReader.MovementValue.x;
+        movement += stateMachine.transform.forward * stateMachine.InputReader.MovementValue.y;
+
+        return movement;
+    }
+
+    private void UpdateAnimator(float deltaTime)
+    {
+        if (stateMachine.InputReader.MovementValue.y == 0)
+        {
+            stateMachine.Animator.SetFloat(TargetingForwardHash, 0, 0.1f, deltaTime);
+        }
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.y > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingForwardHash, value, 0.1f, deltaTime);
+        }
+
+        if (stateMachine.InputReader.MovementValue.x == 0)
+        {
+            stateMachine.Animator.SetFloat(TargetingRightHash, 0);
+        }
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.x > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingRightHash, value, 0.1f, deltaTime);
+        }   
+
     }
 }
